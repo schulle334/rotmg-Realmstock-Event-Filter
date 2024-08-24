3e30bb818e003c7d32a960898f87fdf958da9922
schulle334 <62450965+schulle334@users.noreply.github.com>
2024-08-24 13:20:06 +0200

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,18 +1,15 @@
 document.getElementById('applySettings').addEventListener('click', () => {
     const minScore = parseInt(document.getElementById('minScore').value, 10);
     const minPopulation = parseInt(document.getElementById('minPopulation').value, 10);
+    const maxPopulation = parseInt(document.getElementById('maxPopulation').value, 10);
     const timeLimitMinutes = parseInt(document.getElementById('timeLimitMinutes').value, 10);
 
     chrome.storage.sync.set({
         minScore,
         minPopulation,
+        maxPopulation,
         timeLimitMinutes
     }, () => {
-        alert('Settings saved');
-
-        // Reload the current page
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.tabs.reload(tabs[0].id);
-        });
+        alert('Einstellungen gespeichert');
     });
 });
